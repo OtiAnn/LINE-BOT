8794838a0485937c27122ebe2ce0d4ecd62b7b38
Annie Chen <anniebh30@gmail.com>
2016-07-12 02:13:08 +0800

    Add send sticker to user method

--- app/controllers/line_user_controller.rb
+++ app/controllers/line_user_controller.rb
@@ -3,10 +3,40 @@ class LineUserController < ApplicationController
     params_from_line = params['result']
     if params_from_line.present?
       user_mid = params_from_line[0]['content']['from']
-      LineUser.first_or_create!(user_mid: user_mid)
+      @line_user = LineUser.first_or_create!(user_mid: user_mid)
+      send_sticker_to_user(user_mid: @line_user.user_mid)
+
       head :ok, content_type: "text/html"
     else
       head :bad_request, content_type: "text/html"
+    end
+  end
+
+  private
+  def send_sticker_to_user(user_mid: nil)
+    if user_mid != 'uffffffffffffffffffffffffffffffff'
+      send_content = {
+        'to' => user_mid, 
+        'toChannel' => 1383378250, #fixed
+        'eventType' => '138311608800106203',  #fixed
+        'content' => {
+          'contentType' => 8,
+          'toType' => 1,
+          'contentMetadata' => {
+            'STKID' => ['144','156','164','172','177','501'].sample,
+            'STKPKGID' => '2',
+            'STKVER' => '100' }
+        }
+      }.to_json
+      uri = URI.parse("https://trialbot-api.line.me/v1/events")
+      https = Net::HTTP.new(uri.host,uri.port)
+      https.use_ssl = true
+      req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json; charset=UTF-8'})
+      req.add_field('X-Line-ChannelID', ENV['X-Line-ChannelID'])
+      req.add_field('X-Line-ChannelSecret', ENV['X-Line-ChannelSecret'])
+      req.add_field('X-Line-Trusted-User-With-ACL', ENV['X-Line-Trusted-User-With-ACL'])
+      req.body = send_content
+      res = https.request(req)
     end
   end
 end
