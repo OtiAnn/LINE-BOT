7830c7bc8a55d0970099d79237ae82cf6ac18e1c
Annie Chen <anniebh30@gmail.com>
2016-07-12 01:05:29 +0800

    Add get line api callback method

---
+++ app/controllers/get_api_controller.rb
@@ -0,0 +1,12 @@
+class GetApiController < ApplicationController
+  def line
+    permitted = params.permit('result' => { 'content' => 'from'}
+    if permitted.permitted?
+      user_mid = permitted['result'][0]['content']['from']
+      LineUser.first_or_create!(user_mid: user_mid)
+      render status: 200
+    else
+      render status: 400
+    end
+  end
+end
