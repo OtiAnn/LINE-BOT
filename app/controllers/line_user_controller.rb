aa120198387b10ab4b275fc702f1907b8acfcb88
Annie Chen <anniebh30@gmail.com>
2016-07-12 01:35:26 +0800

    Fix protect_from_forgery to use null_session

--- app/controllers/line_user_controller.rb
+++ app/controllers/line_user_controller.rb
@@ -1,8 +1,8 @@
 class LineUserController < ApplicationController
   def get_api
-    permitted = params.require(:line_user).permit('result' => { 'content' => 'from' })
-    if permitted.permitted?
-      user_mid = permitted['result'][0]['content']['from']
+    params_from_line = params['result']
+    if params_from_line.present?
+      user_mid = params_from_line[0]['content']['from']
       LineUser.first_or_create!(user_mid: user_mid)
       render status: 200
     else
